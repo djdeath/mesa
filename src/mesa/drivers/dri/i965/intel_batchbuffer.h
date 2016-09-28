49872278f4544c209830784d3883f12a2a649a3e
Lionel Landwerlin <lionel.g.landwerlin@intel.com>
2016-09-28 16:18:40 +0100

    Revert "tentative"
    This reverts commit 022da960edbf6ef7f9e70ef8eee6139008db9d16.

--- src/mesa/drivers/dri/i965/intel_batchbuffer.h
+++ src/mesa/drivers/dri/i965/intel_batchbuffer.h
@@ -151,12 +151,6 @@ intel_batchbuffer_advance(struct brw_context *brw)
    uint32_t *__map = brw->batch.map_next;              \
    brw->batch.map_next += (n)
 
-
-#define BEGIN_BATCH(n) do {                            \
-   intel_batchbuffer_begin(brw, (n), RENDER_RING);     \
-   uint32_t *__map = brw->batch.map_next;              \
-   brw->batch.map_next += (n)
-
 #define BEGIN_BATCH_BLT(n) do {                        \
    intel_batchbuffer_begin(brw, (n), BLT_RING);        \
    uint32_t *__map = brw->batch.map_next;              \
@@ -188,61 +182,6 @@ intel_batchbuffer_advance(struct brw_context *brw)
    assert(__map == brw->batch.map_next); \
    intel_batchbuffer_advance(brw);       \
 } while (0)
-
-
-/* Genxml macros: */
-
-struct i965_address {
-   drm_intel_bo *bo;
-   uint32_t offset;
-   uint32_t read_domains;
-   uint32_t write_domain;
-
-};
-
-static inline uint64_t
-_i965_combine_address(struct brw_context *brw, void *location,
-                      const struct i965_address address, uint32_t delta)
-{
-   if (address.bo == NULL) {
-      return address.offset + delta;
-   } else {
-      return intel_batchbuffer_reloc64(brw, address.bo,
-                                       address.offset,
-                                       address.read_domains,
-                                       address.write_domain,
-                                       delta);
-   }
-}
-
-#define __gen_address_type struct i965_address
-#define __gen_user_data struct brw_context
-#define __gen_combine_address _i965_combine_address
-
-#define __i965_cmd_length(cmd) cmd ## _length
-#define __i965_cmd_length_bias(cmd) cmd ## _length_bias
-#define __i965_cmd_header(cmd) cmd ## _header
-#define __i965_cmd_pack(cmd) cmd ## _pack
-//#define __i965_reg_num(reg) reg ## _num
-
-
-#define I965_BATCH_EMIT(cmd, name)                                      \
-   for (struct cmd name = { __i965_cmd_header(cmd) },                   \
-           *_dst = anv_batch_emit_dwords(batch, __i965_cmd_length(cmd)); \
-        __builtin_expect(_dst != NULL, 1);                              \
-        ({ __i965_cmd_pack(cmd)(batch, _dst, &name);                    \
-           VG(VALGRIND_CHECK_MEM_IS_DEFINED(_dst, __i965_cmd_length(cmd) * 4)); \
-           _dst = NULL;                                                 \
-        }))
-
-#define I965_RELOC(buf, r_domains, w_domain, dt)        \
-   (struct i965_address) {                              \
-      .buffer = buf,                                    \
-      .offset = (__map - brw->batch.map) * 4,           \
-      .read_domains = r_domains,                        \
-      .write_domain = w_domain,                         \
-      .delta = dt,                                      \
-   }
 
 #ifdef __cplusplus
 }
