ff40410683660fb17f2cad50ed322685d8f80124
Lionel Landwerlin <lionel.g.landwerlin@intel.com>
2018-05-27 20:33:25 +0100

    i965: extract performance query metrics
    We would like to reuse performance query metrics in other APIs. Let's
    make the query code dealing with the processing of raw counters into
    human readable values API agnostic.
    
    Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>

--- src/mesa/drivers/dri/i965/Android.mk
+++ src/mesa/drivers/dri/i965/Android.mk
@@ -293,7 +293,8 @@ LOCAL_WHOLE_STATIC_LIBRARIES := \
 	libmesa_intel_common \
 	libmesa_isl \
 	libmesa_blorp \
-	libmesa_intel_compiler
+	libmesa_intel_compiler \
+	libmesa_intel_perf
 
 LOCAL_SHARED_LIBRARIES := \
 	$(MESA_DRI_SHARED_LIBRARIES)
